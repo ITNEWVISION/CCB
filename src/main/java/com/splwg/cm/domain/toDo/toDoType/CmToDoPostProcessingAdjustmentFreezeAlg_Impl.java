aaecac224dc629ec43cb8db6358808cc9c4ade66
ITNEWVISION <asvprasadrao@gmail.com>
2016-03-12 10:08:55 +0100

    small update

--- src/main/java/com/splwg/cm/domain/toDo/toDoType/CmToDoPostProcessingAdjustmentFreezeAlg_Impl.java
+++ src/main/java/com/splwg/cm/domain/toDo/toDoType/CmToDoPostProcessingAdjustmentFreezeAlg_Impl.java
@@ -79,7 +79,7 @@ public class CmToDoPostProcessingAdjustmentFreezeAlg_Impl extends
 		if(notNull(oldToDoDto)){
 			// Adjustment adjustment = null;
 			if(!oldToDoDto.getEntity().getDrillKeyValues().isEmpty()){
-				String drilKeyValue = oldToDoDto.getEntity().getDrillKeyValues().iterator().next().getKeyValue123091();
+				String drilKeyValue = oldToDoDto.getEntity().getDrillKeyValues().iterator().next().getKeyValue123();
 				// adjustment = new Adjustment_Id(drilKeyValue).getEntity();
 			}
 			// adjustment.getApprovalRequestId();
