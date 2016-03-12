648aba229fcb35f003f66de96494cbfb17947bb3
hari <hari>
2016-03-12 15:17:27 +0100

    testing1

--- src/main/java/source/cm/com/splwg/cm/domain/toDo/toDoType/CmToDoPostProcessingAdjustmentFreezeAlg_Impl.java
+++ src/main/java/source/cm/com/splwg/cm/domain/toDo/toDoType/CmToDoPostProcessingAdjustmentFreezeAlg_Impl.java
@@ -79,7 +79,7 @@ public class CmToDoPostProcessingAdjustmentFreezeAlg_Impl extends
 		if(notNull(oldToDoDto)){
 			// Adjustment adjustment = null;
 			if(!oldToDoDto.getEntity().getDrillKeyValues().isEmpty()){
-				String drilKeyValue = oldToDoDto.getEntity().getDrillKeyValues().iterator().next().getKeyValue1230912();
+				String drilKeyValue = oldToDoDto.getEntity().getDrillKeyValues().iterator().next().getKeyValue009();
 				// adjustment = new Adjustment_Id(drilKeyValue).getEntity();
 			}
 			// adjustment.getApprovalRequestId();
